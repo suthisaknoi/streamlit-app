1f1d1d2a2da39b25d3ca8d7dfbcdf36e2c606182
suthisaknoi <suthisak.noi@gmail.com>
2025-02-18 10:19:19 +0700

    10.19

---
+++ Public_Sheet_Example.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+st.subheader("📗 Google Sheets st.connection using Public URLs")
+
+url = "https://docs.google.com/spreadsheets/d/1W9_xgTxfXzDutdYKir1UI5VoGTI08TJK1buUmklS2RU/edit?usp=sharing"
+# https://docs.google.com/spreadsheets/d/1W9_xgTxfXzDutdYKir1UI5VoGTI08TJK1buUmklS2RU/edit?usp=sharing
+
+st.write("#### 1. Read public Google Worksheet as Pandas")
+
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    df = conn.read(spreadsheet=url, usecols=[0, 1])
+    st.dataframe(df)
+
+st.write("#### 2. Query public Google Worksheet using SQL")
+st.info(
+    "Mutation SQL queries are in-memory only and do not results in the Worksheet update.",
+    icon="ℹ️",  # noqa: RUF001
+)
+st.warning(
+    """You can query only one Worksheet in provided public Spreadsheet,
+        use Worksheet name as target in from SQL queries.
+        The worksheet, which you query is defined by GID query parameter or GID parameters to query method.""",
+    icon="⚠️",
+)
+
+
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    df = conn.query('select births from "Example 2" limit 10', spreadsheet=url)
+    st.dataframe(df)
